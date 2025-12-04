3f13185c16451ff8662136399225be95ab978ef7
Ruslan <jer_X_2014_projekt@mail.ru>
2025-12-04 17:16:39 +0300

    Mobile version of brands section created

--- js/main.js
+++ js/main.js
@@ -1,39 +1,62 @@
-// Сайдбар
 document.addEventListener("DOMContentLoaded", () => {
-  const burgers = document.querySelectorAll(".burger-icon");
-  const aside = document.querySelector("aside.main__aside");
-  const content = document.querySelector(".main__content");
-  const header = document.querySelector(".header");
-  const body = document.body;
+  // Сайдбар
+  {
+    const burgers = document.querySelectorAll(".burger-icon");
+    const aside = document.querySelector("aside.main__aside");
+    const content = document.querySelector(".main__content");
+    const header = document.querySelector(".header");
+    const body = document.body;
 
-  function toggleSidebar() {
-    aside.classList.toggle("aside--visible");
-    content.classList.toggle("main__content--blured");
-    header.classList.toggle("header--blured");
-    body.classList.toggle("body--event");
-  }
-
-  function closeSidebar() {
-    aside.classList.remove("aside--visible");
-    content.classList.remove("main__content--blured");
-    header.classList.remove("header--blured");
-    body.classList.remove("body--event");
-  }
-
-  burgers.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      e.stopPropagation();
-      toggleSidebar();
-    });
-  });
-
-  aside.addEventListener("click", (e) => {
-    e.stopPropagation();
-  });
-
-  document.addEventListener("click", () => {
-    if (aside.classList.contains("aside--visible")) {
-      closeSidebar();
+    function toggleSidebar() {
+      aside.classList.toggle("aside--visible");
+      content.classList.toggle("main__content--blured");
+      header.classList.toggle("header--blured");
+      body.classList.toggle("body--event");
     }
-  });
+
+    function closeSidebar() {
+      aside.classList.remove("aside--visible");
+      content.classList.remove("main__content--blured");
+      header.classList.remove("header--blured");
+      body.classList.remove("body--event");
+    }
+
+    burgers.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        toggleSidebar();
+      });
+    });
+
+    aside.addEventListener("click", (e) => {
+      e.stopPropagation();
+    });
+
+    document.addEventListener("click", () => {
+      if (aside.classList.contains("aside--visible")) {
+        closeSidebar();
+      }
+    });
+  }
+
+  // Слайдер бренды
+  {
+    const swiper = new Swiper(".brands__slider", {
+      slidesPerView: "auto",
+      spaceBetween: 16,
+
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+
+      autoplay: {
+        delay: 2000,
+      },
+
+      speed: 600,
+
+      pauseOnInteraction: true,
+    });
+  }
 });
